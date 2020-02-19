0b37f3fc69787b9b31c96d2acf5133268944db4b
pramudyawibowo <pramudya.wibowo72@gmail.com>
2020-02-19 21:26:36 +0700

    Perubahan rumus

--- Pertemuan 2/Tugas Jobsheet 2/ProgramLingkaran/src/programlingkaran/Lingkaran.java
+++ Pertemuan 2/Tugas Jobsheet 2/ProgramLingkaran/src/programlingkaran/Lingkaran.java
@@ -2,7 +2,7 @@ package programlingkaran;
 public class Lingkaran {
     public double PHI = 3.14, r;
     public double hitungLuas(){
-        return PHI * r;
+        return PHI * r * r;
     }
     public double hitungKeliling(){
         return PHI * (2 * r);
