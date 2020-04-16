cdf32658f7bb1e8b49f3fbaea2a9378eab7609e7
pramudyawibowo <pramudya.wibowo72@gmail.com>
2020-04-16 12:02:09 +0700

    Pertemuan 11

--- Pertemuan 11/minggu11/src/minggu11/ModifikasiMainLinkedLists.java
+++ Pertemuan 11/minggu11/src/minggu11/ModifikasiMainLinkedLists.java
@@ -24,7 +24,7 @@ public class ModifikasiMainLinkedLists {
             data.removeByValue(4);
             data.print();
             data.clear();
-            int pilih = 0, pilih1, nilai, index;
+            int pilih, pilih1, nilai, index;
             do {
                 System.out.println("\nMenu");
                 System.out.println("1. Tambah");
